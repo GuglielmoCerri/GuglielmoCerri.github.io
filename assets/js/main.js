327a356a138b576f006deeacccf136280cc26a39
GuglielmoCerri <cerriguglielmo@gmail.com>
2023-12-02 11:15:13 +0100

    added google recaptcha inside formspree

--- assets/js/main.js
+++ assets/js/main.js
@@ -186,3 +186,11 @@ themeButton.addEventListener('click', () => {
     localStorage.setItem('selected-theme', getCurrentTheme())
     localStorage.setItem('selected-icon', getCurrentIcon())
 })
+
+// Google recaptcha
+window.onload = function() { 
+    var el = document.getElementById('g-recaptcha-response'); 
+    if (el) { 
+      el.setAttribute('required', 'required'); 
+    } 
+  }
